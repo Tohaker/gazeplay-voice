895fd74cadddfac19fd6230dcfa2df8fe8c89842
milesbardon <milesbardon@gmail.com>
2020-03-16 23:05:37 +0000

    Added travis deployment pipeline

---
+++ deployment/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+printf "\nBegin Deployment of AWS Lambda.\n"
+
+function finish {
+    rv=$?
+    printf "\nDeployment completed with code ${rv}\n"
+}
+
+trap finish EXIT
+
+current_directory=$(dirname $0)
+pushd ${current_directory}
+
+set -e
+
+echo "Initialising Terraform."
+terraform init \
+    -backend=true \
+    -backend-config="access_key=${AWS_ACCESS_KEY_ID}" \
+    -backend-config="secret_key=${AWS_SECRET_ACCESS_KEY}"
+
+echo "Planning Terraform."
+terraform plan \
+    -out=output.tfplan
+
+echo "Applying Terraform."
+terraform apply \
+    -auto-approve \
+    "output.tfplan"
